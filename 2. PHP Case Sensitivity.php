e99cbf5cdd48e1982baa45848220fcae18e60454
viraj-gujarati7 <161927060+viraj-gujarati7@users.noreply.github.com>
2024-03-02 13:49:45 +0530

    Add files via upload

---
+++ 2. PHP Case Sensitivity.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <?php
+        $color = "black";
+        echo "My car is ". $Color . "</br>";
+        echo "My dog is ". $color . "</br>";
+        echo "My Phone is ". $COLOR . "</br>";
+        ?>
+    </body>
+</html>
